6909d73d1e3b7c164f568389f7eb3eecc3da76a7
Samuel Carvalho <samuel.carvalho@monetizze.com.br>
2021-05-22 02:36:16 -0300

    Ajustar doc

--- 176/pkg/cachorro/cachorro.go
+++ 176/pkg/cachorro/cachorro.go
@@ -1,9 +1,7 @@
-//cachorro é o pacote de um tanto de função legal
+// cachorro é o pacote de um tanto de função legal
 package cachorro
 
-//Idade calcula todas idades dos cachorros
+// Idade calcula todas idades dos cachorros
 func Idade(idade int) int {
 	return idade * 7
 }
-
-
